c0ded44c67602af83f6c6168dda66a47605dcf23
Ian Florentino <ianflorentino88@gmail.com>
2014-08-24 23:46:51 -0700

    Updated game logic

--- js/main.js
+++ js/main.js
@@ -4,61 +4,89 @@ app.controller('ticTacController', ['$scope', function($scope){
 	$scope.cells = [["","",""],
 			["","",""],
        			["","",""]];
-       	
-	player1_turn = true;
-	num_check = $scope.cells.length
-	
+       
+	$scope.className  = "none";
+	player1_turn      = true;
+	player1_select    = 0;
+	num_check         = $scope.cells.length;
+	winner            = 0;
+	player1	          = "blue";
+	player2           = "green";	
 
 	$scope.switchTurn = function() {
 		player1_turn = !player1_turn;
 	}
        	
 	$scope.selectBox = function(x,y) {
-		$scope.cells[x][y] = (player1_turn ? 'X' : 'O');
-		$scope.winnerCond();
-		$scope.switchTurn();
+		$scope.cells[x][y] = (player1_turn ? 'X' : 'O');	
+		winnerCond();
+		$scope.checkDiagonal($scope.cells);	
 		$scope.checkWinner($scope.cells);
-		console.log($scope.cells);
-		console.log($scope.cells[x][y]);
+		$scope.switchTurn(); 
+		console.log($scope.cells[0], $scope.cells[1], $scope.cells[2]);
+		
 	}
 	
-	$scope.winnerCond = function() {
+	winnerCond = function() {
 		var choice  = (player1_turn ? 'X' : 'O');
-	       	var winner = choice;
+	       	var winCombo = choice;
 		for (var i = 1; i < num_check; i++){
-			winner += choice
+			winCombo += choice
 		}
-		return winner;
+		return winCombo;
+	}
+
+	$scope.checkDiagonal = function(array) {
+		diagonal_array1 = [];
+		for (var i = 0; i < num_check; i++) {
+			diagonal_array1.push(array[i][i]);
+			if (diagonal_array1.join('') == winnerCond()) {
+				console.log((player1_turn) ? "player 1 wins" : "[layer 2 wins");
+				return;
+			}
+		}
+
+		diagonal_array2 = [];
+		j = num_check - 1			
+		for (var i = 0; i < num_check; i++) {
+			diagonal_array2.push(array[i][j]);
+			j -= 1
+			
+			if (diagonal_array2.join('') == winnerCond()) {
+				console.log((player1_turn) ? "player 1 wins" : "player 2 wins");
+				return;
+			}
+		
+		}
 	}
 
       	$scope.checkWinner = function(array) {	
 		for (var i = 0; i < num_check; i++){
-			if (array[i].join('') == $scope.winnerCond()){
-				console.log("winner found");
+			if (array[i].join('') == winnerCond()){
+				console.log((player1_turn) ? "player 1 wins" : "player 2 wins");
 				return 
 			}
 			console.log(array[i].join(''));
 		};
-			
-		//loop through rows
-		//	loop through array rows
-		//		loop through row of objects
-		//loop through columns
-		//	loop through each array object
-		//		loop through progressively
-		//loop through diagnols
-		//	loop through progressively
-		// 
-		array[0].join('')
-		array[1].join('')
-		array[2].join('')
-		array[0][0] + array[1][0] + array[2][0]
-		array[0][1] + array[1][1] + array[2][1]
-		array[0][2] + array[1][2] + array[2][2]
-		array[0][0] + array[1][1] + array[2][2]
-		array[2][2] + array[1][1] + array[0][0]
-
-		//compare board with winning conditions and assign how many times each 
-		//
+		
+		vertical_array = [];	
+		for (var i = 0; i < num_check; i++) {
+			for (var j = 0; j < num_check; j++) {
+				vertical_array.push(array[j][i]);
+				if (vertical_array.length > num_check) {
+					for (var h = 0; h < num_check; h++) {
+						vertical_array.shift();
+						if (vertical_array.join('') == winnerCond()) {
+							break;
+						}
+					}
+				}
+				if (vertical_array.join('') == winnerCond()) {
+					console.log((player1_turn) ? "player 1 wins" : "player 2 wins");
+					return;
+				}
+			}
+		}
+		
       	}
 }]);
