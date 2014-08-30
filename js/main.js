9445115d213f567ab9f5f579f79ac36fa4cfbb51
Ian Florentino <ianflorentino88@gmail.com>
2014-08-30 12:15:00 -0700

    Fixed responsive design for Mobile

--- js/main.js
+++ js/main.js
@@ -4,85 +4,142 @@ app.controller('ticTacController', ['$scope', '$firebase', function($scope, $fir
 	$scope.box = {cells: 
 			[["","",""],
 			["","",""],
-       			["","",""]], 
+      ["","",""]], 
 			player1_turn:true,
 			player1_score:0, 
 			player2_score:0,
-			winner:false};
-	
-	var ref = $firebase(new Firebase("https://tictacfighter.firebaseio.com/data"));
+			winner:false,
+			tie:false,
+      move:true,
+      moveCounter:0,
+      currPlayer:0};
 
-	ref.$bind($scope, 'box');
-	$scope.$watch('box', function(){
+	var ref = $firebase(new Firebase("https://tictacfighter.firebaseio.com/data"));
+  ref.$bind($scope, 'box');
+	
+  $scope.$watch('box', function(){
 		console.log("player 1 turn is " + $scope.box.player1_turn);
-	});
+  });
 
 	$scope.num_check = $scope.box.cells.length;
-	$scope.move = true;
+  playerCount = 0;
 
-	$scope.ifWin = function() {
-		return $scope.box.winner
-	}
-	
+  onload = function() {
+    $scope.box.currPlayer += 1;
+  }
+
+  $scope.reset = function() {
+    console.log("reset went through");
+    $scope.box.cells= [["","",""],
+    ["","",""],
+    ["","",""]];
+    $scope.box.winner = false;
+    $scope.box.move = true;
+    $scope.box.tie = false;
+  }
+  
+  $scope.ifWin = function() {
+    if ($scope.box.winner) {
+      return $scope.box.winner;
+    } else if ($scope.box.tie) {
+      return $scope.box.tie;
+    }
+  };
+   
 	$scope.switchTurn = function() {
-		if ($scope.box.winner == false) {
-			$scope.box.player1_turn = !$scope.box.player1_turn;
-			console.log("switched turns");
-		}
-	}
+    if ($scope.box.winner == false) {
+		  $scope.box.player1_turn = !$scope.box.player1_turn;
+      $scope.box.moveCounter += 1;
+      playerCount = $scope.box.moveCounter;
+    }
+	};
 
 	$scope.isRed = function(x,y) {
 		console.log($scope.box.cells[x][y]);
-		if ($scope.box.cells[x][y] === 'X') {
-			return 'white';
+    if ($scope.box.cells[x][y] === 'X') {
+      return 'white';
 		} else if ($scope.box.cells[x][y] === 'O') {
-			return 'red';
+      return 'red';
 		}
-	}
+	};
 	
-	$scope.animate = function() {
-		console.log($scope.move);
-		$scope.move = !$scope.move;
-		if ($scope.move) {
+	$scope.animate = function() {	
+		$scope.box.move = !$scope.box.move;
+		if ($scope.box.move) {
 			return 'animate'
-		} else {
+		} 
+    else {
 			return ''
 		}
-	}
-
+	};
+  
 	$scope.isWinner = function() {
-		if ($scope.box.winner == true) {
-			(player1_turn) ? $scope.box.player1_score +=1 : $scope.box.player1_score +=1;	
-		}	
-	}
+		if ($scope.box.winner) {
+			($scope.box.player1_turn) ? $scope.box.player1_score +=1 : $scope.box.player2_score +=1;	
+		} 
+	};
 
 	$scope.selectBox = function(x,y) {
 		if ($scope.box.cells[x][y] != "") {
 			alert("That spot is already taken!");
 			return
-		}else if ($scope.box.winner == true) {
-			alert("Play another game!");
+		}
+    else if ($scope.box.winner == true) {
+      alert("Play another game!");
 			return
 		}
-		$scope.box.cells[x][y] = ($scope.box.player1_turn ? 'X' : 'O');	
+    else if (!$scope.box.player1_turn) {
+      if (playerCount % 2 == 1) {
+        alert("You cannot go twice in a row!");
+        return
+      }
+    }
+    else if ($scope.box.player1_turn) {
+      if (playerCount != 0 && playerCount % 2 == 0) {
+        alert("You cannot go twice in a row!");
+        return
+      }
+    }
+    
+    console.log($scope.box.move);
+    $scope.box.cells[x][y] = ($scope.box.player1_turn ? 'X' : 'O');	
 		$scope.animate();
 		$scope.winnerCond();
-		console.log("winner combo:" + $scope.winnerCond());
+    console.log($scope.box.move);
+    console.log("winner combo:" + $scope.winnerCond());
 		$scope.checkDiagonal($scope.box.cells);	
 		$scope.checkWinner($scope.box.cells);
-		$scope.switchTurn();
-		console.log($scope.box.cells[0], $scope.box.cells[1], $scope.box.cells[2]);
-		
-	}
+		$scope.isWinner();
+    $scope.switchTurn();
+    $scope.tieCombo();
+		console.log($scope.box.cells[0], $scope.box.cells[1], $scope.box.cells[2]);	
+	};
 	
 	$scope.winnerCond = function() {
 		var choice  = ($scope.box.player1_turn ? 'X' : 'O');
-	       	var winCombo = choice;
+	    var winCombo = choice;
 		for (var i = 1; i < $scope.num_check; i++){
 			winCombo += choice
 		}
 		return winCombo;
-	}
+	};
+  
+  $scope.tieCombo = function() {
+    var filledSpaces = 9;
+    if ($scope.box.tie == true || $scope.box.winner == true) {
+      filledSpaces = 0;
+    }
+    for (var i = 0; i < $scope.num_check; i++){
+      for (var y = 0; y < $scope.num_check; y++) {
+        if ($scope.box.cells[i][y] != '') {
+          filledSpaces -= 1;
+          if (filledSpaces == 0) {
+            $scope.box.tie = true;
+          }
+        }
+      }
+    }  
+  }
 
 	$scope.checkDiagonal = function(array) {
 		diagonal_array1 = [];
@@ -108,10 +165,10 @@ app.controller('ticTacController', ['$scope', '$firebase', function($scope, $fir
 			}
 		
 		}
-	}
+	};
 
-      	$scope.checkWinner = function(array) {	
-		for (var i = 0; i < $scope.num_check; i++){
+  $scope.checkWinner = function(array) {	
+	  for (var i = 0; i < $scope.num_check; i++){
 			if (array[i].join('') == $scope.winnerCond()){
 				$scope.box.winner = true;
 				console.log(($scope.player1_turn) ? "player 1 wins:checkwinner:row" : "player 2 wins:checkwinner:row");
@@ -135,5 +192,5 @@ app.controller('ticTacController', ['$scope', '$firebase', function($scope, $fir
 			
 		}
 		
-      	}
+  };
 }]);
